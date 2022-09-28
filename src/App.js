16167b6f89c7e59122f0438d4dcbc7cd9f5bfee1
mahmud035 <mahamudulhasan4148@gmail.com>
2022-09-29 04:17:00 +0600

    added accordian and dummy data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './Components/Home/Home';
 import Navigation from './Components/Navigation/Navigation';
+import QuestionAndAnswer from './Components/QuestionAndAnswer/QuestionAndAnswer';
 
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
       <Home></Home>
+      <QuestionAndAnswer></QuestionAndAnswer>
     </div>
   );
 }
---
+++ src/Components/QuestionAndAnswer/QuestionAndAnswer.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './QuestionAndAnswer.css';
+import Accordion from 'react-bootstrap/Accordion';
+
+const QuestionAndAnswer = () => {
+  return (
+    <div className="accordion-container">
+      <Accordion className="w-50 mx-auto accordion">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>How Does React Works?</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header className="accordion-header">
+            Difference between props and state.
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>
+            What does useEffect do except data fetching?
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
+};
+
+export default QuestionAndAnswer;
