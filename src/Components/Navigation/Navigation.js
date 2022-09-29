ce874611c812c39ddf563af91c389b2cec13cab4
mahmud035 <mahamudulhasan4148@gmail.com>
2022-09-29 07:17:40 +0600

    added nav-menu item

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -5,7 +5,7 @@ import './Navigation.css';
 
 const Navigation = () => {
   return (
-    <Navbar className="py-3 navigation-bar">
+    <Navbar className="py-3 navigation-bar" expand="md">
       <Container>
         <Navbar.Brand>
           <FaDumbbell size={32} className="nav-icon"></FaDumbbell>
@@ -13,7 +13,20 @@ const Navigation = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto navbar-cart-container"></Nav>
+          <Nav className="ms-auto nav-item">
+            <Nav.Link href="#home" className="nav-link">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#link" className="nav-link">
+              Services
+            </Nav.Link>
+            <Nav.Link href="#link" className="nav-link">
+              About
+            </Nav.Link>
+            <Nav.Link href="#link" className="nav-link">
+              Contact
+            </Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
